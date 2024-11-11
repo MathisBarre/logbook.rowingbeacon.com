b2605936d17dec098a6bdc044d6d00cdd9f6b799
Mathis Barré <mathis.barre@live.fr>
2024-11-11 20:07:45 +0100

    remove useless usage of tanstack query

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles.css";
-import { MyQueryClientProvider } from "./_common/lib/tanstack-query";
 import { Router } from "./Router";
 import { Toaster } from "sonner";
 import { WindowConfirm } from "./_common/components/WindowConfirm";
@@ -10,12 +9,10 @@ import { WindowAlert } from "./_common/components/WindowAlert";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <MyQueryClientProvider>
-      <Router />
-      <Toaster position="bottom-center" richColors closeButton />
-      <WindowConfirm />
-      <WindowPrompt />
-      <WindowAlert />
-    </MyQueryClientProvider>
+    <Router />
+    <Toaster position="bottom-center" richColors closeButton />
+    <WindowConfirm />
+    <WindowPrompt />
+    <WindowAlert />
   </React.StrictMode>
 );
