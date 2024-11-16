45d7fac0697f8474efb0a11bfa650a712a99bbf3
Mathis Barré <mathis.barre@live.fr>
2024-11-16 17:22:10 +0100

    persist incidents

--- src/_common/store/incident.store.ts
+++ src/_common/store/incident.store.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 interface ZustandIncident {
   id: string;
@@ -13,18 +14,25 @@ interface ZustandIncidentStore {
   getIncidents: () => ZustandIncident[];
 }
 
-const useIncidentStore = create<ZustandIncidentStore>((set, get) => ({
-  incidents: [],
-  addIncident: (incident: ZustandIncident) => {
-    set((state) => ({
-      incidents: [incident, ...state.incidents],
-    }));
-  },
-  getIncidents: () => {
-    return get().incidents.sort(
-      (a, b) => b.datetime.getTime() - a.datetime.getTime()
-    );
-  },
-}));
+const useIncidentStore = create(
+  persist<ZustandIncidentStore>(
+    (set, get) => ({
+      incidents: [],
+      addIncident: (incident: ZustandIncident) => {
+        set((state) => ({
+          incidents: [incident, ...state.incidents],
+        }));
+      },
+      getIncidents: () => {
+        return get().incidents.sort(
+          (a, b) => b.datetime.getTime() - a.datetime.getTime()
+        );
+      },
+    }),
+    {
+      name: "incidents",
+    }
+  )
+);
 
 export default useIncidentStore;
