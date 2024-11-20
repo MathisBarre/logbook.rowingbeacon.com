37ee26e2cc4ce43438035912d104c3671b1b26c4
Mathis Barré <mathis.barre@live.fr>
2024-11-20 17:32:05 +0100

    add sqlite support

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,8 +2,8 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-    tauri::Builder
-        ::default()
+    tauri::Builder::default()
+        .plugin(tauri_plugin_sql::Builder::new().build())
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_sql::Builder::default().build())
         .run(tauri::generate_context!())
