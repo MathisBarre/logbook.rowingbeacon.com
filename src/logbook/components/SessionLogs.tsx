e4b7bfd009ea1396264d90aed0712bbfddf7d504
Mathis Barré <mathis.barre@live.fr>
2024-11-21 16:57:54 +0100

    remove export buttons

--- src/logbook/components/SessionLogs.tsx
+++ src/logbook/components/SessionLogs.tsx
@@ -1,8 +1,5 @@
 import Button from "../../_common/components/Button";
-import {
-  ZustandSession,
-  useSessionsStore,
-} from "../../_common/store/sessions.store";
+import { ZustandSession } from "../../_common/store/sessions.store";
 import { SessionHistoryTable, SessionInTable } from "./SessionLogsTable";
 import { useState } from "react";
 import { ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
@@ -14,14 +11,8 @@ import { Boat } from "../../_common/types/boat.type";
 import { generateSessionId } from "../../_common/business/session.rules";
 import { addHours } from "../../_common/utils/date.utils";
 import { useClubOverviewStore } from "../../_common/store/clubOverview.store";
-import { windowConfirm } from "../../_common/utils/window.utils";
-import {
-  askForAdminPassword,
-  useAdminEditModeSystem,
-} from "../../_common/store/adminEditMode.system";
 
 export const SessionLogs = () => {
-  const sessionsStore = useSessionsStore();
   const clubOverviewStore = useClubOverviewStore();
 
   const [currentPage, setCurrentPage] = useState(1);
@@ -35,66 +26,10 @@ export const SessionLogs = () => {
 
   const numberOfPages = Math.ceil(formattedData.length / pageSize) || 1;
 
-  const adminEditSystem = useAdminEditModeSystem();
-
   return (
     <div className="flex-1 shadow-md bg-white flex flex-col absolute inset-0 right-1/2 mr-[.125rem] rounded overflow-hidden">
       <div className="bg-border p-2 bg-steel-blue-900 text-white flex justify-between h-12 items-center">
         <h1 className="text-base ml-2">Historique des sorties</h1>
-        <div className="flex gap-2">
-          <a
-            href={
-              "data:text/json;charset=utf-8," +
-              encodeURIComponent(JSON.stringify(sessionsStore.session))
-            }
-            download={"export.json"}
-            className="bg-gray-100 rounded flex items-center justify-center px-4 text-gray-700 py-1"
-          >
-            Export JSON
-          </a>
-          <a
-            href={
-              "data:text;charset=utf-8," +
-              encodeURIComponent(`
-                id,boatName,startDateTime,endDateTime,routeName,comment,rowersName
-                ${sessionsStore.session
-                  .map(
-                    (sess) =>
-                      `${sess.id},${sess.boat.name},${sess.startDateTime},${
-                        sess.endDateTime
-                      },${sess.route.name},${sess.comment},${sess.rowers
-                        .map((r) => r.name)
-                        .join(";")}`
-                  )
-                  .join("\n")
-                  .replace(" ", "")}
-                `)
-            }
-            download={"export.csv"}
-            className="bg-gray-100 rounded flex items-center justify-center px-4 text-gray-700 py-1"
-          >
-            Export CSV
-          </a>
-          <button
-            className="bg-error-100 rounded flex items-center justify-center px-4 text-error-700 py-1"
-            onClick={async () => {
-              if (
-                await windowConfirm(
-                  "Êtes-vous sûr de vouloir supprimer toutes les sessions ?"
-                )
-              ) {
-                if (
-                  adminEditSystem.allowAdminActions(await askForAdminPassword())
-                ) {
-                  sessionsStore.reset();
-                  setCurrentPage(1);
-                }
-              }
-            }}
-          >
-            Supprimer l'historique
-          </button>
-        </div>
       </div>
 
       <SessionHistoryTable sessionsInTableList={paginatedData} />
