8f09aaff39cc26b83cc8ac768fd0d98b00d7af6a
Mathis Barré <mathis.barre@live.fr>
2024-11-21 16:58:51 +0100

    remove devtools

--- src/logbook/components/SessionLogs.tsx
+++ src/logbook/components/SessionLogs.tsx
@@ -4,17 +4,8 @@ import { SessionHistoryTable, SessionInTable } from "./SessionLogsTable";
 import { useState } from "react";
 import { ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
 import { paginateData } from "../../_common/utils/pagination.utils";
-import { isDev } from "../../_common/utils/utils";
-import { Rower } from "../../_common/types/rower.type";
-import { Route } from "../../_common/types/route.type";
-import { Boat } from "../../_common/types/boat.type";
-import { generateSessionId } from "../../_common/business/session.rules";
-import { addHours } from "../../_common/utils/date.utils";
-import { useClubOverviewStore } from "../../_common/store/clubOverview.store";
 
 export const SessionLogs = () => {
-  const clubOverviewStore = useClubOverviewStore();
-
   const [currentPage, setCurrentPage] = useState(1);
   const pageSize = 25;
   const formattedData = formatData(sessionsStore.session);
@@ -61,81 +52,8 @@ export const SessionLogs = () => {
           </Button>
         </div>
       </section>
-
-      {isDev && (
-        <p className="absolute bg-white py-1 px-2 border border-gray-300 bottom-4 right-4 rounded-sm flex gap-2 items-center ">
-          devtools:session-generator:1000{" "}
-          <button
-            className="bg-steel-blue-300 px-2 py-1"
-            onClick={() => {
-              console.log("click");
-              const generatedSessions = generateSessions(
-                clubOverviewStore.clubOverview.rowers,
-                clubOverviewStore.clubOverview.boats,
-                clubOverviewStore.clubOverview.routes,
-                1000
-              );
-
-              sessionsStore.bulkAddSessions(generatedSessions);
-            }}
-          >
-            generate
-          </button>
-        </p>
-      )}
     </div>
   );
-};
-
-const generateSessions = (
-  rowers: Rower[],
-  boats: Boat[],
-  routes: Route[],
-  nb: number
-) => {
-  const sessions = [];
-  for (let i = 0; i < nb; i++) {
-    // get a random boat in boat list
-    const randomBoat = boats[Math.floor(Math.random() * boats.length)];
-
-    // get a random route in route list
-    const randomRoute = routes[Math.floor(Math.random() * routes.length)];
-
-    // get x rowers depending of boat type
-    const numberOfRowersToGet = {
-      ONE_ROWER_COXLESS: 1,
-      TWO_ROWERS_COXLESS: 2,
-      TWO_ROWERS_COXED: 2,
-      FOUR_ROWERS_COXLESS: 4,
-      FOUR_ROWERS_COXED: 4,
-      EIGHT_ROWERS_COXED: 8,
-      OTHER: 1,
-    };
-
-    const rowersToGet =
-      numberOfRowersToGet[randomBoat.type || "ONE_ROWER_COXLESS"];
-
-    const sRowers = [];
-
-    for (let i = 0; i < rowersToGet; i++) {
-      sRowers.push(rowers[Math.floor(Math.random() * rowers.length)]);
-    }
-
-    const session = {
-      id: generateSessionId(),
-      rowers: sRowers,
-      route: randomRoute,
-      boat: randomBoat,
-      startDateTime: new Date().toISOString(),
-      estimatedEndDateTime: new Date().toISOString(),
-      comment: "RAS",
-      endDateTime: addHours(new Date(), 2).toISOString(),
-    };
-
-    sessions.push(session);
-  }
-  console.log(sessions);
-  return sessions;
 };
 
 const formatData = (
