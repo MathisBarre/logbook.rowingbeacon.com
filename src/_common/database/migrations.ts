67a4c87ea20237dbb0405fa4cc23b60c34512f04
Mathis Barré <mathis.barre@live.fr>
2024-11-21 12:52:42 +0100

    add indexes

--- src/_common/database/migrations.ts
+++ src/_common/database/migrations.ts
@@ -1,8 +1,13 @@
 import createSessionTable from "./migrations/01_create-session-table";
 import updateSessionTable from "./migrations/02_update-session-table";
 import createSessionRowersTable from "./migrations/03_session_rowers-table";
+import addIndexes from "./migrations/04_add-index";
 
-export const migrations = [
+export const migrations: {
+  label: string;
+  timestamp: number;
+  sql: string;
+}[] = [
   {
     label: "Create session table",
     timestamp: 1732188036770,
@@ -17,5 +22,10 @@ export const migrations = [
     label: "Session rowers table",
     timestamp: 1732189665154,
     sql: createSessionRowersTable,
+  },
+  {
+    label: "Add indexes to improve performance on existing tables",
+    timestamp: 1732189896438,
+    sql: addIndexes,
   },
 ];
