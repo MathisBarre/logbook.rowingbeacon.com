8302907c6dc521ac7ed4e65042d72fc28c9a4d9c
Mathis Barré <mathis.barre@live.fr>
2025-10-28 15:50:10 +0100

    simpler translations

--- src/_common/i18n/config.ts
+++ src/_common/i18n/config.ts
@@ -3,9 +3,6 @@ import { initReactI18next } from "react-i18next";
 import { Locale } from "../store/locale.store";
 import enUS from "./locales/en-US.json";
 import enGB from "./locales/en-GB.json";
-import enCA from "./locales/en-CA.json";
-import enAU from "./locales/en-AU.json";
-import enNZ from "./locales/en-NZ.json";
 import frFR from "./locales/fr-FR.json";
 import deDE from "./locales/de-DE.json";
 import itIT from "./locales/it-IT.json";
@@ -14,9 +11,9 @@ import nlNL from "./locales/nl-NL.json";
 const resources = {
   "en-US": { translation: enUS },
   "en-GB": { translation: enGB },
-  "en-CA": { translation: enCA },
-  "en-AU": { translation: enAU },
-  "en-NZ": { translation: enNZ },
+  "en-CA": { translation: enGB },
+  "en-AU": { translation: enGB },
+  "en-NZ": { translation: enGB },
   "fr-FR": { translation: frFR },
   "de-DE": { translation: deDE },
   "it-IT": { translation: itIT },
